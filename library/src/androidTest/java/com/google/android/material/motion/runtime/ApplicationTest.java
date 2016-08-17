539ac4e1497e8d76d49c0ca9f72d11643f106638
seanoshea <oshea.ie@gmail.com>
2016-08-17 17:00:59 -0400

    Per issue #4 - Trying to get the build to succeed again.

--- library/src/androidTest/java/com/google/android/material/motion/runtime/ApplicationTest.java
+++ library/src/androidTest/java/com/google/android/material/motion/runtime/ApplicationTest.java
@@ -27,8 +27,5 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
   public ApplicationTest() {
     super(Application.class);
   }
-
-  public void testIntentionalFailure() {
-    assertTrue(1 == 0);
-  }
+  
 }
